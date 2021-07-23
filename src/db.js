f57fc7ead02c15f064c91d9505ab12ee31cb69ec
MaryGeorge07 <87664383+MaryGeorge07@users.noreply.github.com>
2021-07-23 13:22:17 +0530

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/summerschool', {useNewUrlParser: true, useUnifiedTopology: true}
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true}
 ).then(()=>{console.log("Database connected")})
 .catch((e)=>{console.log("Database connection error: " + e)})
